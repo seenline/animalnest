d58f0a619baa7e04e9d8a88e5df6225a3124e00d
seenline <1078657808@qq.com>
2021-08-04 23:07:22 +0800

    commit: 环境初始化

---
+++ src/main/java/com/spca/animalnest/AnimalnestApplication.java
@@ -0,0 +1,13 @@
+package com.spca.animalnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalnestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalnestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spca/animalnest/AnimalnestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spca.animalnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalnestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
