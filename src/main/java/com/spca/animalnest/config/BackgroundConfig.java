0e60b388652b4a80fc92449eda65f62a03ff2522
seenline <1078657808@qq.com>
2021-08-27 20:45:25 +0800

    commit: 阿里云oss交互工具类

--- src/main/java/com/spca/animalnest/config/BackgroundConfig.java
+++ src/main/java/com/spca/animalnest/config/BackgroundConfig.java
@@ -3,22 +3,33 @@ package com.spca.animalnest.config;
 import lombok.Data;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Data
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "background")
 public class BackgroundConfig implements InitializingBean {
 
     public static String ACCESS_KEY1;
     public static String ACCESS_KEY2;
     private String appId;
-    private String appSecret;
+    private String appSecret; //微信端帐号主体密钥
+
+    public static String ACCESS_KEY3;
+    public static String ACCESS_KEY4;
+    public static String ACCESS_KEY5;
+    private String aliId;
+    private String aliKey;
+    private String bucketName;//阿里云帐号主体密钥与实例
 
     @Override
     public void afterPropertiesSet() throws Exception {
         ACCESS_KEY1 = appId;
         ACCESS_KEY2 = appSecret;
+        ACCESS_KEY3=aliId;
+        ACCESS_KEY4=aliKey;
+        ACCESS_KEY5=bucketName;
     }
 }
 
---
+++ src/main/java/com/spca/animalnest/tool/OssInteraction.java
@@ -0,0 +1,46 @@
+package com.spca.animalnest.tool;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSClientBuilder;
+import org.junit.Test;
+
+import javax.management.ObjectName;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Date;
+
+/**
+ * @author seenline
+ * @data 2021/8/26 9:51
+ * @description 与阿里云oss的交互
+ */
+
+public class OssInteraction {
+    @Test
+    public static String push(String name,InputStream inputStream,String type) {
+        String objectNameChoice[] = {"pictures/"+name,"videos/"+name};
+        String objectName;
+
+
+        String endpoint = "https://oss-cn-beijing.aliyuncs.com";
+        String accessKeyId = "LTAI5t9vmToJ2GxGr2ZUqMVz";
+        String accessKeySecret = "mDOVDDWzPBR2VJ4xB0igccsPC6FOYa";
+        String bucketName = "test-to-zsy";
+
+        if (type.equals("picture")) {
+            objectName = objectNameChoice[0];
+        }
+        else
+            objectName=objectNameChoice[1];
+
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+        ossClient.putObject(bucketName, objectName,inputStream);
+        String URL=ossClient.generatePresignedUrl(bucketName,objectName, new Date(new Date().getTime()+3600l*1000*24*365*10)).toString();
+        ossClient.shutdown();
+        return URL;
+    }
+
+}
